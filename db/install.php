381699dc21768ebfd48f1e5ffa7680250239f510
Luke Carrier <luke@carrier.im>
2014-09-01 12:19:44 +0100

    Move INSERT statements out of install.xml
    Fixes #1

---
+++ db/install.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * Moodle/Totara LMS CPD plugin.
+ *
+ * A modern replacement for the legacy report_cpd module, which has numerous
+ * security issues. Supports the existing tables without any fuss.
+ *
+ * @author Luke Carrier <luke@carrier.im> <luke@tdm.co>
+ * @copyright 2014 Luke Carrier, The Development Manager Ltd
+ * @license GPL v3
+ */
+
+/**
+ * XMLDB installation function.
+ *
+ * This function is called by XMLDB post-table creation during the installation
+ * process, and is used to populate the created tables with data.
+ *
+ * For safety and code footprint reasons, we avoid using our model classes here.
+ *
+ * @return boolean Always true.
+ */
+function xmldb_local_cpd_install() {
+    global $DB;
+
+    $activitytypes = array(
+        array('Attendence in college/university'),
+        array('Computer based training'),
+        array('Conferences'),
+        array('Discussions'),
+        array('Examination'),
+        array('Individual informal study'),
+        array('Mentoring'),
+        array('On-the-job training'),
+        array('Professional Institute'),
+        array('Reading'),
+        array('Self-managed learning'),
+        array('Seminars'),
+        array('Structured discussions'),
+        array('Training course'),
+    );
+    foreach ($activitytypes as $activitytype) {
+        $DB->insert_record('cpd_activity_type', (object) array(
+            'name' => $activitytype[0],
+        ));
+    }
+
+    $statuses = array(
+        array('Started',       2),
+        array('Objective met', 3),
+        array('Not started',   1),
+    );
+    foreach ($statuses as $status) {
+        $DB->insert_record('cpd_status', (object) array(
+            'name'          => $status[0],
+            'display_order' => $status[1],
+        ));
+    }
+
+    $years = array(
+        array(1262304000, 1293839999),
+        array(1293840000, 1325375999),
+        array(1325376000, 1356998399),
+    );
+    foreach ($years as $year) {
+        $DB->insert_record('cpd_year', (object) array(
+            'startdate' => $year[0],
+            'enddate'   => $year[1],
+        ));
+    }
+
+    return true;
+}
